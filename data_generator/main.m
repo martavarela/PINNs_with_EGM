e2c6b84da6b6d7363ef61c60dbbc6a117799eea4
Nurin <147408722+nurinmohdamrilshah@users.noreply.github.com>
2025-06-09 19:43:16 +0100

    Add files via upload

--- data_generator/main.m
+++ data_generator/main.m
@@ -7,12 +7,7 @@ param = set_AP_parameter(false,false);
 %% Aliev Panfilov stimulation model
 [Vsav,filepath] = AlievPanfilov2D(param);
 %% EGM Calculation
+% clc;
 phie = calcphie(param,Vsav,filepath);
-
-%%
-figure;
-hold on
-plot(squeeze(Vsav(5,5,:)))
-plot(squeeze(Vsav(50,50,:)))
 
 
